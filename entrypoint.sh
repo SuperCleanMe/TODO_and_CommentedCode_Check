28f9533eb00ce171dbcf6b298c65ddc7d00b2892
Nick <nickswimz@gmail.com>
2020-07-02 16:33:05 -0400

    todo and code check added 	modified:   entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,28 @@
 #!/bin/sh -l
 
+# default passed is true, if fails checks then passed is false
+passed=1
+
+
+todos=$(egrep -rn "TODO|todo|to-do" *)
+case $? in
+    0) echo $todos && passed=0;;
+    *) echo "No TODO's found";;
+esac
+
+
 commented_code=$(egrep -rn  "# " *.py > a && egrep -rn "#" *.py > b && diff -c a b | grep  "+" && rm a && rm b)
-#echo $? # 2 is no .py # 1 is no commented code 0 is commented code
+# 2 is no .py # 1 is no commented code 0 is commented code
 
 case $? in
-    1) echo "No commented out code found" && exit 0 ;;
-    2) echo "No .py files to check in project" && exit 0 ;;
-    0) echo $commented_code && exit 1 ;;
+    1) echo "No commented out code found" ;;
+    2) echo "No .py files to check in project" ;;
+    0) echo $commented_code && passed=0 ;;
 esac
-    
+
+case $passed in
+    0) exit 1;;
+    1) exit 0;;
+esac
+
+
