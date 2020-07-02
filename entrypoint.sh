d40426c4a43cedd32e0642a6a5b02b0af7f937ea
Nick <nickswimz@gmail.com>
2020-07-02 16:25:21 -0400

    should be working check 	modified:   entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -l
 
-ls
+commented_code=$(egrep -rn  "# " *.py > a && egrep -rn "#" *.py > b && diff -c a b | grep  "+" && rm a && rm b)
+#echo $? # 2 is no .py # 1 is no commented code 0 is commented code
 
-echo $(ls)
+case $? in
+    1) echo "No commented out code found" && exit 0 ;;
+    2) echo "No .py files to check in project" && exit 0 ;;
+    0) echo $commented_code && exit 1 ;;
+esac
+    
